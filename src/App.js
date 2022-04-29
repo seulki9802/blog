b7f115c54c2cca77efac0c729228fddfd34efbe6
seulki9802 <calop@naver.com>
2022-04-29 18:47:01 +0900

    setState 배열 추가해서 post추가하기

--- src/App.js
+++ src/App.js
@@ -1,36 +1,84 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import List from './List';
 import Wrapper from './Wrapper';
 import InputSample from './InputSample';
 import './App.css';
 import seulki from './src_assets/seulki.JPG';
+import CreatePost from './CeatePost';
 
 function App() {
 
-  let [show, setShow] = useState({ isShow : false, buttonText : '리스트 보기' })
-  function listShow() {
-    var cloneShow = {...show};
-    cloneShow.isShow = !cloneShow.isShow;
-    if (cloneShow.isShow) cloneShow.buttonText = '리스트 닫기'
-    else cloneShow.buttonText = '리스트 보기'
+  let [posts, setPosts] = useState([
+    {
+      title : '나주 전망대',
+      date : '7월 8일',
+      id : 0
+    },
+    {
+      title : '전주 삼겹살 맛집',
+      date : '11월 3일',
+      id : 1
+    },
+    {
+      title : '군산 장미 칼국수',
+      date : '3월 1일',
+      id : 2
+    }
+  ]);
 
-    setShow(cloneShow)
+  let nextId = useRef(3);
+
+  let [inputs, setInputs] = useState({
+    title: '',
+    date: ''
+  })
+
+  let onChange = e => {
+    let { className, value } = e.target;
+    setInputs({
+      ...inputs,
+      [className]: value //title(or date) : 입력 값!
+    })
+  }
+
+  let onCreate = () => {
+
+    var post = {
+      title : inputs.title,
+      date : inputs.date,
+      id : nextId.current
+    }
+
+    // setPosts([...posts, post]);
+    setPosts(posts.concat(post));
+    //concat은 기존 배열에 추가해주는 함수
+
+    setInputs({
+      title: '',
+      date: ''
+    });
+    nextId.current += 1;
   }
 
   return (
     <div className="App">
 
-      <div className="black-nav">
-        개발개발 블로그 blog BLOG Blog
-      </div>
-
-      <img src={ seulki } className="image-size" alt="seulki" /><br/>
-      <InputSample />
-
+      <Navbar />
+      <Me />
 
       <Wrapper>
-        <button onClick={ listShow }>{ show.buttonText }</button>
-        <List name="suelki's list" isShow={ show.isShow }/>
+        <InputSample />
+      </Wrapper>
+
+      <CreatePost
+        title = { inputs.title }
+        date = { inputs.date }
+        onChange = { onChange }
+        onCreate = { onCreate }
+      />
+
+      <Wrapper>
+        <List postsState={ [posts, setPosts] } />
       </Wrapper>
 
       <Modal />
@@ -39,15 +87,29 @@ function App() {
   );
 }
 
+function Navbar() {
+  return(
+    <div className="black-nav">
+      개발개발 블로그 blog BLOG Blog
+    </div>
+  )
+}
+
+function Me() {
+  return(
+    <div>
+      <img src={ seulki } className="image-size" alt="seulki" />
+    </div>
+  )
+}
+
 function Modal() {
   return(
-    <>
     <div className='modal'>
       <h2>title</h2>
       <p>date</p>
       <p>detail</p>
     </div>
-    </>
   )
 }
 
